7be0c8e4f16e1be4c0f0f7f63578bb9f5950f82f
Sergey Petrunin <neyasut@gmail.com>
2017-01-17 20:20:40 +0200

    add methods implementation to money storage

--- models/moneystorage.go
+++ models/moneystorage.go
@@ -1,8 +1,10 @@
 package models
 
+import "errors"
+
 // The MoneyStorage describes item for sale
 type MoneyStorage struct {
-	cashBackAmount, moneyAmount, userDeposit, currentDeposit float32
+	moneyAmount, userDeposit, currentDeposit float32
 }
 
 func NewMoneyStorage(cashBackAmount float32) *MoneyStorage {
@@ -11,13 +13,23 @@ func NewMoneyStorage(cashBackAmount float32) *MoneyStorage {
 
 // AddMoney allows user to deposit money
 func (m *MoneyStorage) AddMoney(amount float32) {
-
+	m.userDeposit += amount
 }
 
-func (m *MoneyStorage) DeductMoney(amount float32) error {
-	return nil
+func (m *MoneyStorage) UseMoney(amount float32) (float32, error) {
+	if m.userDeposit-amount < 0 {
+		return m.userDeposit, errors.New("Not enough money")
+	}
+
+	m.userDeposit -= amount
+	m.moneyAmount += amount
+
+	return m.userDeposit, nil
 }
 
-func (m *MoneyStorage) MakeCashBack() (float32, error) {
-	return 0, nil
+func (m *MoneyStorage) MakeCashBack() (cashback float32) {
+	cashback = m.userDeposit
+	m.userDeposit = 0
+
+	return cashback
 }
